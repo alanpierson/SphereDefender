4d24824ef5cb60296854beb274330ce1fee58407
alanpierson <a.pierson182@gmail.com>
2016-08-29 19:23:04 -0400

    UE4 Project Created

---
+++ SphereDefender/Source/SphereDefender/SphereDefenderGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SphereDefenderGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPHEREDEFENDER_API ASphereDefenderGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
