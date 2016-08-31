1682f3a58b1644a31fcad31dd322b5a86c97d03e
alanpierson <a.pierson182@gmail.com>
2016-08-31 18:26:08 -0400

    Slider Bar Working

--- SphereDefender/Source/SphereDefender/MainPlayer.cpp
+++ SphereDefender/Source/SphereDefender/MainPlayer.cpp
@@ -6,6 +6,7 @@ AMainPlayer::AMainPlayer()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	/*TOP DOWN VIEW*/
 	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->AttachTo(RootComponent);
@@ -26,34 +27,18 @@ void AMainPlayer::SetupPlayerInputComponent(class UInputComponent* InputComponen
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 
+	/*Allows phone gyroscope to control shield rotation. Disabled for now*/
+	//InputComponent->BindVectorAxis("RotationRate", this, &AMainPlayer::OnRotationInput);
 	
-	//InputComponent->BindAxis("Rotate", this, &AMainPlayer::Rotate);
-	InputComponent->BindVectorAxis("RotationRate", this, &AMainPlayer::OnRotationInput);
-	InputComponent->BindAxis("Up_Down", this, &AMainPlayer::UpDown);
-	InputComponent->BindAxis("Left_Right", this, &AMainPlayer::LeftRight);
+	/*Allows traditional WASD navigation. Disabled for now*/
+	//InputComponent->BindAxis("Up_Down", this, &AMainPlayer::UpDown);
+	//InputComponent->BindAxis("Left_Right", this, &AMainPlayer::LeftRight);
 }
 
-/*
-void AMainPlayer::Rotate(float val)
-{
-	AddControllerYawInput(val);
-}
-*/
-
+/* DISABLED FEATURES
 void AMainPlayer::OnRotationInput(FVector Input)
 {
 
-	//static float DEG_TO_RAD = PI / (180.f);
-
-	//TODO: get right and down scale factor from player controller to make the game playable for people who don't want to do full body motions, like while sitting
-
-	//FQuat RightRot = FQuat(FVector(0.f, 0.f, -1.f), Input.Y * DEG_TO_RAD);		//Why is right reversed, I don't know...  Damnit MATH!!!!
-	//FQuat DownRot = FQuat(FVector(0.f, 1.f, 0.f), Input.X * DEG_TO_RAD);
-	//FQuat RollLeftRot = FQuat(FVector(1.f, 0.f, 0.f), Input.Z * DEG_TO_RAD);
-
-	//FQuat Result = GetActorQuat() * RightRot * DownRot * RollLeftRot;
-
-	//SetActorRotation(Result.Rotator());
 	float val = 0.f;
 	if (Input.X > 1.f || Input.X < -1.f)
 	{
@@ -61,6 +46,7 @@ void AMainPlayer::OnRotationInput(FVector Input)
 		AddControllerYawInput(val);
 	}
 }
+
 
 void AMainPlayer::UpDown(float val)
 {
@@ -73,4 +59,5 @@ void AMainPlayer::LeftRight(float val)
 	FVector Right(0, 1, 0);
 	AddMovementInput(Right, val);
 }
+*/
 
